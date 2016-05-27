abb5c254faaae424e72f560923abf037152afa1b
Bkliger <bkliger@comcast.net>
2016-05-27 10:38:54 -0700

    basic slider

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,14 +1,10 @@
 $(function(){
   $("#save_answers").click(function( event ) {
     alert($('#Stereo > input:radio[name=options]:checked').val());
-    // alert($("#testRadio input[name=options]:checked").val());
-
-    // if ($("#option2").is(":checked")) {
-    //       alert("option2 checked");
-    //    }
-    //  else {
-    //       alert("option2 not checked");
-    //    }
     event.preventDefault();
   });
+
+  $('#slider').slider({
+  animate: "fast"});
+
 });
