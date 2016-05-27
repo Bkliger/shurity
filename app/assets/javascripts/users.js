f4094e0c9fd08d187bb2b9dbafe7c019e113446e
Bkliger <bkliger@comcast.net>
2016-05-27 14:33:36 -0700

    succesfully retrieved values from questions

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,27 @@
 $(function(){
   $("#save_answers").click(function( event ) {
-    alert($('#Stereo > input:radio[name=options]:checked').val());
+    $( ".question" ).each(function() {
+      if (this.id === "radio"){
+        alert($('#radio > input:radio[name=options]:checked').val());
+        alert($(this).data("id"));
+      }
+      else {
+        alert($('#slider').slider('getValue'));
+      }
+});
+    $('#Stereo > input:radio[name=options]:checked').val();
+    $('#slider').slider('getValue');
+// $('input[id="sliderDivId"]').slider('getValue')
+    // .slider('getValue')
     event.preventDefault();
   });
 
-  $('#slider').slider({
-  animate: "fast"});
+  $("#slider").slider(
+    {
+    ticks: [0, 100, 200, 300, 400],
+    ticks_labels: ['$0', '$100', '$200', '$300', '$400'],
+    ticks_snap_bounds: 30
+  });
+
 
 });
