41bbeeae71bac875704003874508178b38859a56
Bkliger <bkliger@comcast.net>
2016-05-28 09:58:45 -0700

    start

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,29 @@ Question.destroy_all
 Category.destroy_all
 User.destroy_all
 
-user1=User.create(first_name:"joe",last_name:"blow")
+# user1=User.create(first_name:"joe",last_name:"blow")
 
 cat_1 = Category.create(name: 'Bluetooth Speaker')
 Category.create(name: 'Over-Ear Headphones')
 Category.create(name: 'Home Theater Audio')
 
-question_1 = Question.create(category_id: cat_1.id, text: 'Color', presentation_type: 'drop_down')
-question_2 = Question.create(category_id: cat_1.id, text: 'Stereo', presentation_type: 'radio')
-question_3 = Question.create(category_id: cat_1.id, text: 'Budget', presentation_type: 'slider')
 
+question_1 = Question.create(category_id: cat_1.id, text: 'Do the speakers need to be waterproof?', presentation_type: 'reg-slider')
+question_2 = Question.create(category_id: cat_1.id, text: 'Is portability important?', presentation_type: 'reg-slider')
+question_3 = Question.create(category_id: cat_1.id, text: 'How important is sound quality to you?', presentation_type: 'reg-slider')
+question_4 = Question.create(category_id: cat_1.id, text: 'Speakerphone capable?', presentation_type: 'radio')
+question_5 = Question.create(category_id: cat_1.id, text: 'How important is durability?', presentation_type: 'reg-slider')
+question_6 = Question.create(category_id: cat_1.id, text: 'Does the sound need to go in all directions?', presentation_type: 'radio')
+question_7 = Question.create(category_id: cat_1.id, text: 'Is Volume important', presentation_type: 'reg-slider')
+question_8 = Question.create(category_id: cat_1.id, text: 'Battery Duration', presentation_type: 'reg-slider')
+question_9 = Question.create(category_id: cat_1.id, text: 'What is your maximum budget?', presentation_type: 'slider')
+question_10 = Question.create(category_id: cat_1.id, text: 'Color', presentation_type: 'drop_down')
 
-Option.create(question_id: question_1.id, opt_value: 'Red')
-Option.create(question_id: question_1.id, opt_value: 'Black')
-Option.create(question_id: question_2.id, opt_value: 'Stereo')
-Option.create(question_id: question_2.id, opt_value: 'Mono')
-Option.create(question_id: question_3.id, opt_value: 'Budget')
+Option.create(question_id: question_10.id, opt_value: 'Red')
+Option.create(question_id: question_10.id, opt_value: 'Black')
+Option.create(question_id: question_4.id, opt_value: 'Yes')
+Option.create(question_id: question_4.id, opt_value: 'No')
+Option.create(question_id: question_6.id, opt_value: 'Yes')
+Option.create(question_id: question_6.id, opt_value: 'No')
 
-Answer.create(category_id:cat_1.id, user_id: user1.id, question_id: question_2.id)
+# Answer.create(category_id:cat_1.id, user_id: user1.id, question_id: question_2.id)
