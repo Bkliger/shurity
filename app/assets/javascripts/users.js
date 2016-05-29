0d92a60f27fc657a7f6d90dfc9dfaa6933269c07
Bkliger <bkliger@comcast.net>
2016-05-28 22:25:42 -0700

    successfully saved slider information

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,22 +1,26 @@
-$(function(){
-  $("#save_answers").click(function( event ) {
-      var category = $("#cat_questions").data("category");
-    $( ".question" ).each(function() {
-      if (this.id === "radio"){
-        alert($('#radio > input:radio[name=options]:checked').val());
-        alert($(this).data("text"));
-        alert($(this).data("id"));
-      }
-      else {
-        alert($('#slider').slider('getValue'));
-      }
-});
-    $('#Stereo > input:radio[name=options]:checked').val();
-    $('#slider').slider('getValue');
-// $('input[id="sliderDivId"]').slider('getValue')
-    // .slider('getValue')
-    event.preventDefault();
-  });
-  
+// $(function(){
+//   $(".save-answers").click(function( event ) {
+//
+//     $( ".answer-field" ).each(function() {
+//
+//
+//       }
+//       else {
+//         alert($('#slider').slider('getValue'));
+//       }
+// });
+//     $('#Stereo > input:radio[name=options]:checked').val();
+//     $('#slider').slider('getValue');
+// // $('input[id="sliderDivId"]').slider('getValue')
+//     // .slider('getValue')
+//     event.preventDefault();
+//   });
+//
+//
+// });
 
-});
+
+      // var category = $("#cat_questions").data("category");
+      // alert($('#radio > input:radio[name=options]:checked').val());
+      // alert($(this).data("text"));
+      // alert($(this).data("id"));
