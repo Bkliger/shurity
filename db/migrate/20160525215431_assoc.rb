68fea5e7b21aaa9e7b1f28250391d92b30ef221d
Bkliger <bkliger@comcast.net>
2016-05-31 14:51:43 -0700

    fixed migration for heroku

--- db/migrate/20160525215431_assoc.rb
+++ db/migrate/20160525215431_assoc.rb
@@ -1,9 +1,9 @@
 class Assoc < ActiveRecord::Migration
   def change
 
-    change_table :questions do |t|
-      t.belongs_to :category, index:true
-    end
+    # change_table :questions do |t|
+    #   t.belongs_to :category, index:true
+    # end
 
     change_table :options do |t|
       t.belongs_to :question, index:true
