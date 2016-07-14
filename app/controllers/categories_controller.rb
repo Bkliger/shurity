e7d8536b74dfb211f903abc5b7068133c5fa406f
Bkliger <bkliger@comcast.net>
2016-07-14 16:21:56 -0700

    selectCat still incomplete - added full crud for Categories - unformatted

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,56 @@ class CategoriesController < ApplicationController
     @cat = Category.all
 
   end
+
+  def new
+    @cat = Category.new
+  end
+
+  def create
+
+    @cat = Category.create(category_params)
+          redirect_to catList_path
+  end
+
+  def show
+    @cat = Category.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @cat = Category.find(params[:id])
+    render :edit
+  end
+
+  def update
+      @cat = Category.find params[:id]
+    @cat.update_attributes(category_params)
+      redirect_to catList_path
+  end
+
+  def destroy
+
+    category = Category.find_by_id(params[:id])
+    category.destroy
+      redirect_to catList_path
+  end
+
+  def get_categories
+   @cat = Category.where("lower(name) LIKE ?", params[:catString].downcase + "%")
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def list
+    @cat = Category.all
+    render :catList
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,20 @@ Rails.application.routes.draw do
 
   devise_for :users
   root  to: "categories#index"
+  get "/categories/new", to:'categories#new', as: "new_category"
+  post "categories", to:"categories#create"
+  get "/categories/:id", to:'categories#show', as: 'category'
+  get '/categories/', to:'categories#list', as: 'catList'
+  get '/categories/:id/edit', to: 'categories#edit', as: 'category_edit'
+  patch '/categories/:id', to: 'categories#update'
+  delete '/categories/:id', to: 'categories#destroy'
+# route for category drop-down
+  get '/categories/get_categories', to: 'categories#get_categories', as:'get_categories'
 
-  get  "/categories/:cat_id/answers/new",       to: "answers#new",        as: "new_answer"
-  post "/answers", to: "answers#create"
+  get  "/categories/:cat_id/answers/new", to: 'answers#new',   as: "new_answer"
+  post "/answers", to: 'answers#create'
   get "/users/:user_id/answers/edit", to: "answers#edit", as: "edit_answers"
   put "/answers/update", to: "answers#update"
-
 
   get "/about", to: "users#about", as: "about"
 
@@ -26,7 +34,7 @@ Rails.application.routes.draw do
 
 end
 #
-#
+# Devise Routes----------------------------------
 # new_user_session GET    /users/sign_in(.:format)                                              devise/sessions#new
 #      user_session POST   /users/sign_in(.:format)                                              devise/sessions#create
 # destroy_user_session DELETE /users/sign_out(.:format)                                             devise/sessions#destroy
