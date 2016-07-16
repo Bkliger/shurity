3db76e25b318c59c81be2a88b8ebf55a584c779a
Bkliger <bkliger@comcast.net>
2016-07-15 17:18:42 -0700

    changes for heroku

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,3 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require bootstrap-slider
+//= require_tree .
+
+
 $(document).ready(function() {
 
   var lastentry = "";
